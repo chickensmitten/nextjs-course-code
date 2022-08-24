2fe83a44c4f4febf0e41520a74065e8eb10f21af
sheng yeong <chia.shengyeong@gmail.com>
2022-08-24 14:45:21 +0800

    extracting path data from dynamic path with useRouter and router.query

--- pages/portfolio/[projectid].js
+++ pages/portfolio/[projectid].js
@@ -1,5 +1,13 @@
+import { userRouter, withRouter } from "next/router";
 
 function PortfolioProjectPage() {
+  const router = useRouter();
+
+  console.log(router.pathname)
+  console.log(router.query)
+
+  // we can then request to some backend server with router.query. i.e. finding the object with id from dynamic routes
+
   return (
     <div>
       <h1>The Portfolio Project Page</h1>
