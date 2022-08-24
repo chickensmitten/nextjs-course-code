8b85500c0284465617a84af80d7a5fdb72dbbc0e
sheng yeong <chia.shengyeong@gmail.com>
2022-08-24 15:12:31 +0800

    navigating dynamic routes

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,8 +1,21 @@
+import Link from "next/link";
 
 function ClientsPage() {
+  const clients = [
+    {id: "max", name: "Maximillian"},
+    {id: "mink", name: "Minksy"}
+  ];
+
   return(
     <div>
       <h1>Client Page</h1>
+      <ul>
+        { clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>          
+        ))}
+      </ul>
     </div>
   )
 }
