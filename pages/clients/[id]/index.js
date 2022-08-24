d931cf076d7967424fa8b6a28f359a4bdbf93eef
sheng yeong <chia.shengyeong@gmail.com>
2022-08-24 15:22:51 +0800

    another way of pogrammatically navigating dynamic routes

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,8 +1,22 @@
+import { useRouter } from 'next/router';
 
 function ClientProjectsPage() {
+  const router = useRouter();
+
+  console.log(router.query);
+
+  function loadProjectHandler() {
+    // load data...
+    router.push({
+      pathname: '/clients/[id]/[clientprojectid]',
+      query: { id: 'max', clientprojectid: 'projecta'}
+    })
+  }
+
   return (
     <div>
       <h1>The projects of a given client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   )
 }
